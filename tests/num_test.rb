67c0b71d6547ee03abed5a13ecc2e4f25aa562e6
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-12 18:40:35 -0400

    fail, doesn't convert

--- tests/num_test.rb
+++ tests/num_test.rb
@@ -1,10 +1,14 @@
 require "minitest/autorun"
-require_relative "num_function.rb"
+require_relative "../num_function.rb"
 
-class TestCoinChanging < Minitest::Test
+class TestNumeralConversion < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
+    end
+
+    def test_that_numbers_change_to_numerals
+        assert_equal("I", num_convert_one(1))
     end
 
 end
