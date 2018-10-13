55661a150d22eff3b5e4e7295160d3859c8a8f3d
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-13 17:18:08 -0400

    pass, result is string

--- num_function.rb
+++ num_function.rb
@@ -1,5 +1,16 @@
 def num_convert_one(number)
-   
+   numerals = {
+       1 => "I",
+       5 => "V",
+       10 => "X",
+       50 => "L",
+       100 => "C",
+       500 => "D",
+       1000 => "M"
+    }
+    result = ""
+
+    result
 end
 
 def num_convert_two(number)
