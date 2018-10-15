9ce4b5afc806bbf3e4daaa07571f7fcf94134b8f
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-15 16:12:58 -0400

    six works

--- tests/num2_test.rb
+++ tests/num2_test.rb
@@ -20,6 +20,7 @@ class TestNumberConversion < Minitest::Test
     def test_that_5_converts
         assert_equal(4, "IV".de_romanizer())
         assert_equal(5, "V".de_romanizer())
+        assert_equal(6, "VI".de_romanizer())
     end
 
 end
