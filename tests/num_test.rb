d4efb4c315d21ccc85ebb93b3e4eaf60842f1791
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-15 10:59:49 -0400

    also works with 3

--- tests/num_test.rb
+++ tests/num_test.rb
@@ -14,6 +14,7 @@ class TestNumeralConversion < Minitest::Test
     def test_that_one_converts
         assert_equal("I", 1.romanizer())
         assert_equal("II", 2.romanizer())
+        assert_equal("III", 3.romanizer())
     end
 
 end
