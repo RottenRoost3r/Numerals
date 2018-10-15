10f182d95f85965f6226c96dfda94ba1af3d8adb
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-15 11:14:45 -0400

    pass, 4 is iv

--- num_function.rb
+++ num_function.rb
@@ -2,13 +2,14 @@ class Integer
 
     def romanizer()
         numerals = {
-           "I" => 1
+            "IV" => 4,
+            "I" => 1
         }
         x = self
         roman = ""
         numerals.each do |key, value|
-            roman << key * x
-            
+            roman << key * (x/value)
+            x %= value
         end
         roman
     end
